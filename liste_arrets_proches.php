7056d40232c37a9b2e16239fb4acaff8434628ad
LUCASDALLACOSTA <lucas.dallacosta@limayrac.fr>
2023-09-04 23:37:42 +0200

    liste gare proches

--- liste_arrets_proches.php
+++ liste_arrets_proches.php
@@ -1,3 +1,129 @@
+<style>
+    /* Reset CSS */
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    /* Global Styles */
+    body {
+        font-family: Arial, sans-serif;
+        overflow: hidden;
+    }
+
+    .container {
+        width: 100%;
+        max-width: 960px;
+        margin: 0 auto;
+        padding: 20px;
+    }
+
+    /* Header Styles */
+    #header {
+        background-color: #333;
+        color: #fff;
+        padding: 10px 0;
+    }
+
+    #header .container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    #header ul {
+        list-style: none;
+        display: flex;
+    }
+
+    #header ul li {
+        margin-right: 10px;
+    }
+
+    #header ul li a {
+        color: #fff;
+        text-decoration: none;
+        padding: 5px;
+    }
+
+    #header ul li a:hover {
+        color: #ffd700;
+    }
+
+    /* Content Styles */
+    h1,
+    h2
+    {
+        text-align: center;
+        margin: 20px 0;
+        background-color: #f8f8f8;
+    }
+
+    form {
+        margin: 20px auto;
+        max-width: 400px;
+        padding: 20px;
+        border: 1px solid #ccc;
+    }
+
+    label {
+        display: block;
+        margin-bottom: 10px;
+        font-weight: bold;
+    }
+
+    input,
+    select {
+        width: 100%;
+        padding: 10px;
+        margin-bottom: 20px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+    }
+
+    input[type="submit"] {
+        background-color: #333;
+        color: #fff;
+        cursor: pointer;
+    }
+
+    input[type="submit"]:hover {
+        background-color: #ffd700;
+    }
+    
+    .logo {
+            width: 50px;
+            height: auto;
+            margin-right: 10px;
+        }
+
+    .success-message {
+        color: green;
+    }
+
+    .error-message {
+        color: red;
+    }
+
+</style>
+
+<head>
+    <title>Arret à proximité</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <header id="header" class="fixed-top ">
+        <div class="container d-flex align-items-center">
+            <img src="assets/img/logo.png" alt="Logo" class="logo">
+            <ul>
+                <li><a href="./">&nbsp;Accueil</a></li>
+                <li><a href="connexion.php">&nbsp;Connexion</a></li>
+                <li><a href="carte.php">&nbsp;Carte</a></li>
+                <li><a href="ajout_csv.php">&nbsp;Ajout via csv</a></li>
+            </ul>
+        </div>
+    </header>
+</head>
+
 <?php
 // Chemin vers le fichier GeoJSON
 $geojson_file = 'includes/transport/stations_de_metro.geojson';
@@ -71,7 +197,7 @@ if (isset($_GET['latitude']) && isset($_GET['longitude'])) {
         });
         
         // Affichez les stations de métro proches sous forme de tableau
-        echo '<h1>Stations de métro à moins de 1 km de la position :</h1>';
+        echo '<h1>Stations de métro à moins de 1 km du POI :</h1>';
         if (!empty($stations_proches)) {
             echo '<table border="1">';
             echo '<tr><th>Nom de la station</th><th>Lignes</th><th>Distance (mètres)</th></tr>';
@@ -92,4 +218,153 @@ if (isset($_GET['latitude']) && isset($_GET['longitude'])) {
 } else {
     echo 'Les paramètres de latitude et de longitude sont manquants dans l\'URL.';
 }
+?>
+
+
+
+<!-- ARRET DE BUS !-->
+
+
+
+<?php
+// Chemin vers le fichier GeoJSON des arrêts de bus
+$geojson_file_bus = 'includes/transport/arrets_de_bus.geojson';
+
+// Vérifiez si les paramètres de latitude et de longitude sont présents dans l'URL
+if (isset($_GET['latitude']) && isset($_GET['longitude'])) {
+    // Récupérez les valeurs de latitude et de longitude depuis l'URL
+    $latitude = floatval($_GET['latitude']);
+    $longitude = floatval($_GET['longitude']);
+
+    // Chargez le contenu du fichier GeoJSON des arrêts de bus
+    $geojson_bus = file_get_contents($geojson_file_bus);
+
+    if ($geojson_bus !== false) {
+        // Convertir le GeoJSON en tableau PHP
+        $data_bus = json_decode($geojson_bus, true);
+
+        // Liste des arrêts de bus à moins de 300 mètres
+        $arrets_proches_bus = array();
+
+        // Distance maximale (en mètres) pour considérer un arrêt comme proche
+        $distance_max_bus = 300;
+
+        // Parcourez les arrêts de bus et vérifiez si ils sont à moins de 300 mètres
+        foreach ($data_bus['features'] as $feature) {
+            $lon_bus = $feature['properties']['geo_point_2d']['lon'];
+            $lat_bus = $feature['properties']['geo_point_2d']['lat'];
+            $distance_bus = calculerDistance($latitude, $longitude, $lat_bus, $lon_bus);
+
+            if ($distance_bus <= $distance_max_bus) {
+                $nom_bus = $feature['properties']['nom_log'];
+                $lignes_bus = $feature['properties']['conc_ligne'];
+
+                $arrets_proches_bus[] = array(
+                    'nom' => $nom_bus,
+                    'lignes' => $lignes_bus,
+                    'distance' => $distance_bus
+                );
+            }
+        }
+
+        // Triez le tableau des arrêts de bus proches par distance croissante
+        usort($arrets_proches_bus, function ($a, $b) {
+            return $a['distance'] - $b['distance'];
+        });
+
+        // Affichez les arrêts de bus proches sous forme de tableau
+        echo '<h1>Arrêts de bus Tisséo à moins de 300 mètres du POI :</h1>';
+        if (!empty($arrets_proches_bus)) {
+            echo '<table border="1">';
+            echo '<tr><th>Nom de l\'arrêt</th><th>Lignes de bus</th><th>Distance (mètres)</th></tr>';
+            foreach ($arrets_proches_bus as $arret_proche_bus) {
+                echo '<tr>';
+                echo '<td>' . $arret_proche_bus['nom'] . '</td>';
+                echo '<td>' . $arret_proche_bus['lignes'] . '</td>';
+                echo '<td>' . round($arret_proche_bus['distance'], 0) . '</td>';
+                echo '</tr>';
+            }
+            echo '</table>';
+        } else {
+            echo 'Aucun arrêt de bus à moins de 300 mètres de la position spécifiée.';
+        }
+    } else {
+        echo 'Erreur lors de la lecture du fichier GeoJSON des arrêts de bus.';
+    }
+} else {
+    echo 'Les paramètres de latitude et de longitude sont manquants dans l\'URL.';
+}
+?>
+
+
+
+<!-- GARE !-->
+
+
+
+<?php
+
+$geojson_file_gares = 'includes/transport/gare_sncf_haute_garonne.geojson';
+
+// Vérifiez si les paramètres de latitude et de longitude sont présents dans l'URL
+if (isset($_GET['latitude']) && isset($_GET['longitude'])) {
+    // Récupérez les valeurs de latitude et de longitude depuis l'URL
+    $latitude = floatval($_GET['latitude']);
+    $longitude = floatval($_GET['longitude']);
+
+    // Chargez le contenu du fichier GeoJSON des gares SNCF
+    $geojson_gares = file_get_contents($geojson_file_gares);
+
+    if ($geojson_gares !== false) {
+        // Convertir le GeoJSON en tableau PHP
+        $data_gares = json_decode($geojson_gares, true);
+
+        // Tableau pour stocker les informations sur les gares proches
+        $gares_proches = array();
+
+        // Nombre de gares à afficher (dans ce cas, 2 gares les plus proches)
+        $nombre_de_gares = 2;
+
+        // Parcourez les gares et calculez les distances
+        foreach ($data_gares['features'] as $feature) {
+            $lon_gare = $feature['geometry']['coordinates'][0];
+            $lat_gare = $feature['geometry']['coordinates'][1];
+            $distance_gare = calculerDistance($latitude, $longitude, $lat_gare, $lon_gare);
+
+            // Ajoutez les informations sur la gare au tableau
+            $gares_proches[] = array(
+                'nom' => $feature['properties']['gare_alias_libelle_noncontraint'],
+                'departement' => $feature['properties']['departement_libellemin'],
+                'distance' => $distance_gare
+            );
+        }
+
+        // Triez le tableau des gares proches par distance croissante
+        usort($gares_proches, function ($a, $b) {
+            return $a['distance'] - $b['distance'];
+        });
+
+        // Affichez les deux gares les plus proches
+        echo '<h1>Les deux gares SNCF les plus proches :</h1>';
+        if (count($gares_proches) >= $nombre_de_gares) {
+            echo '<table border=1>';
+            echo '<tr><th>Nom de la gare</th><th>Département</th><th>Distance (mètres)</th></tr>';
+            for ($i = 0; $i < $nombre_de_gares; $i++) {
+                echo '<tr>';
+                echo '<td>' . $gares_proches[$i]['nom'] . '</td>';
+                echo '<td>' . $gares_proches[$i]['departement'] . '</td>';
+                echo '<td>' . round($gares_proches[$i]['distance'], 0) . '</td>';
+                echo '</tr>';
+            }
+            echo '</table>';
+        } else {
+            echo 'Il n\'y a pas suffisamment de gares proches.';
+        }
+    } else {
+        echo 'Erreur lors de la lecture du fichier GeoJSON des gares SNCF.';
+    }
+} else {
+    echo 'Les paramètres de latitude et de longitude sont manquants dans l\'URL.';
+}
+
 ?>
